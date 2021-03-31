8f5e2ef428b9ee254a2306cf4ad5ef52b8c31828
Shinya Todaka <tdk0105.strawberry@gmail.com>
2021-03-31 19:15:22 +0900

    Initial Commit

---
+++ helloworld_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld_iOS
+//
+//  Created by 戸高新也 on 2021/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloworld_iOS/helloworld_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworld_iOSApp.swift
+//  helloworld_iOS
+//
+//  Created by 戸高新也 on 2021/03/31.
+//
+
+import SwiftUI
+
+@main
+struct helloworld_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
